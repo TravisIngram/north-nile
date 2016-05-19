5d07d1e43a66d9ff056d19d1b51d2fd22cddb61f
TravisIngram <ingramtravis@gmail.com>
2016-05-19 12:16:29 -0500

    Add Audio schema to mediaModel.js

--- server/db/mediaModel.js
+++ server/db/mediaModel.js
@@ -1,6 +1,29 @@
 var pg            = require('pg');
 var dbConnection  = 'postgres://localhost:5432/North_Nile';
 
+function createAudio() {
+  pg.connect(dbConnection, function(err, client, done) {
+    if(err) {
+      console.log('Error connecting to DB.');
+      process.exit(1);
+    } else {
+      var query = client.query('CREATE TABLE IF NOT EXISTS "Audio" (' +
+        ' "id" serial PRIMARY KEY,' +
+        ' "audioReference" varchar(75) NOT NULL,)');
+
+      query.on('end', function() {
+        console.log('Successfully created Audio schema.');
+        done();
+      });
+
+      query.on('error', function(error) {
+        console.log('Error creating Audio schema.' + err);
+        process.exit(1);
+      });
+    }
+  });
+}
+
 function createImage() {
   pg.connect(dbConnection, function(err, client, done) {
     if(err) {
@@ -24,4 +47,5 @@ function createImage() {
   });
 }
 
+module.exports.createImage = createAudio;
 module.exports.createImage = createImage;
