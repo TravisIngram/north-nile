388e0c04a0f7078cba23ecf12551eb5fdc033bc5
sasham43 <sashasemail@gmail.com>
2016-05-19 13:52:55 -0500

    Add leaflet-directive module to client.js

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('northApp', ['ngRoute']);
+var app = angular.module('northApp', ['ngRoute', 'leaflet-directive']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $routeProvider
