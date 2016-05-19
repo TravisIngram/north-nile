c6325b0f48491a41bbd97b87501ec960b4fe8b25
TravisIngram <ingramtravis@gmail.com>
2016-05-19 11:53:35 -0500

    Set up DB connection, Image schema in mediaModel.js

---
+++ server/db/mediaModel.js
@@ -0,0 +1,27 @@
+var pg            = require('pg');
+var dbConnection  = 'postgres://localhost:5432/North_Nile';
+
+function createImage() {
+  pg.connect(dbConnection, function(err, client, done) {
+    if(err) {
+      console.log('Error connecting to DB.');
+      process.exit(1);
+    } else {
+      var query = client.query('CREATE TABLE IF NOT EXISTS "Image" (' +
+        ' "id" serial PRIMARY KEY,' +
+        ' "imageReference" varchar(75) NOT NULL,)');
+
+      query.on('end', function() {
+        console.log('Successfully created Image schema.');
+        done();
+      });
+
+      query.on('error', function(error) {
+        console.log('Error creating Image schema.' + err);
+        process.exit(1);
+      });
+    }
+  });
+}
+
+module.exports.createImage = createImage;
