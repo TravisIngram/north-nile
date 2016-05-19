f2a63c3b737c37418e0049f2eb66af08412e093a
sasham43 <sashasemail@gmail.com>
2016-05-19 13:16:16 -0500

    Change index to serve index.html
    Res.send is now serving index.html instead of home.html.  Angular routes will serve home.html, which is a partial.

--- server/routes/indexRouter.js
+++ server/routes/indexRouter.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 
 router.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../public/views/home.html'))
+  res.sendFile(path.join(__dirname, '../public/views/index.html'))
   console.log('indexRouter is loading');
 });
 
