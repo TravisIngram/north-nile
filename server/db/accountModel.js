f9a8a194cba2a82b67e71993f040d7f3a88073f1
TravisIngram <ingramtravis@gmail.com>
2016-05-19 11:17:16 -0500

    Set up DB connection, Account schema

---
+++ server/db/accountModel.js
@@ -0,0 +1,30 @@
+var pg            = require('pg');
+var dbConnection  = 'postgres://localhost:5432/North_Nile';
+
+function createAccount() {
+  pg.connect(dbConnection, function(err, client, done) {
+    if(err) {
+      console.log('Error connecting to DB.');
+      process.exit(1);
+    } else {
+      var query = client.query('CREATE TABLE IF NOT EXISTS "Account" (' +
+        ' "id" serial PRIMARY KEY,' +
+        ' "userName" varchar(25) NOT NULL,' +
+        ' "password" varchar(75) NOT NULL,' +
+        ' "emailAddress" varchar(50) NOT NULL,' +
+        ' "contactName" varchar(50) NOT NULL,)');
+
+      query.on('end', function() {
+        console.log('Successfully created Account schema.');
+        done();
+      });
+
+      query.on('error', function(error) {
+        console.log('Error creating Account schema.');
+        process.exit(1);
+      });
+    }
+  });
+}
+
+module.exports.createAccount = createAccount;
