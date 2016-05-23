b4294228f6cbaa548e98da16e8a1f9d584ba4444
sasham43 <sashasemail@gmail.com>
2016-05-23 15:08:22 -0500

    Add custom css for infoDrawer transitions, also implemented md-swipe-down for closing infoDrawer

--- client/mapController.js
+++ client/mapController.js
@@ -92,21 +92,23 @@ angular.module('northApp').controller('MapController', ['$scope', 'leafletData',
         markers: mc.visibleMarkers
     });
 
+    // open infoDrawer on marker Click
     $scope.$on('leafletDirectiveMarker.map.click', function(event, args){
       // console.log('clicked a marker:', args, '|event:', event);
       // console.log('visibleMarkers on click:', mc.visibleMarkers);
 
-      // mc.showInfoDrawer = true;
-      mc.dummyText1 = 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.';
-      mc.dummyText2 = 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.';
-      mc.dummyText3 = 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.';
-
+      mc.showInfoDrawer = true;
       mc.lastClicked = args.model;
       mc.markerTitle = mc.lastClicked.title;
       // mc.mapStyle = {height:'20vh'};
-      $mdBottomSheet.show({
-        template: '<md-bottom-sheet><h2>' + mc.markerTitle + '</h2><p>' + mc.dummyText1 + '</p><p>' + mc.dummyText2 + '</p><p>' + mc.dummyText3 + '</p></md-bottom-sheet>'
-      });
+
+      // Angular material bottom sheet testing
+      mc.dummyText1 = 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.';
+      mc.dummyText2 = 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.';
+      mc.dummyText3 = 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.';
+      // $mdBottomSheet.show({
+      //   template: '<md-bottom-sheet><md-content stopTouchEvent><h2>' + mc.markerTitle + '</h2><p>' + mc.dummyText1 + '</p><p>' + mc.dummyText2 + '</p><p>' + mc.dummyText3 + '</p></md-content></md-bottom-sheet>'
+      // });
 
       // update map size
       leafletData.getMap().then(function(map) {
@@ -125,7 +127,8 @@ angular.module('northApp').controller('MapController', ['$scope', 'leafletData',
       });
     });
 
-    $scope.$on('leafletDirectiveMap.map.click', function(event, args){
+    // close infoDrawer on map click
+    mc.closeInfoDrawer = function(event, args){
       mc.showInfoDrawer = false;
 
       mc.mapStyle = {height:'100vh'};
@@ -142,15 +145,20 @@ angular.module('northApp').controller('MapController', ['$scope', 'leafletData',
           zoom: 15
         }
       });
-    });
-
-    $scope.$on('leafletDirectiveMap.map.resize', function(event, args){
-      console.log('resized map');
-    });
-
-    // testing
-
+    }
+    $scope.$on('leafletDirectiveMap.map.click', mc.closeInfoDrawer());
 
   mc.filterMarkers('all');
   console.log('Map controller loaded.');
 }]);
+
+angular.module('northApp').directive('stopTouchEvent', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element.on('touchmove', function (evt) {
+                evt.stopPropagation();
+            });
+        }
+    };
+})
