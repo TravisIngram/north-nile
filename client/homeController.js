5951a470cf90d3035dbdf56b7c9ec227a2feb11b
Amy Jennings <amyjenningsmn@gmail.com>
2016-05-23 14:49:06 -0500

    Deleted commented out logout function in homeController

--- client/homeController.js
+++ client/homeController.js
@@ -106,28 +106,6 @@ hc.registerUser = function() {
     hc.registerInfo.username = undefined;
   });
 };
-// hc.logOutUser=function(){
-// $http.post('/logout', hc.loginInfo).then(function(response){
-// if(response.status==200){
-//   console.log('successfully logged out', response.data.isAdmin);
-// if(response.data.isAdmin==true){
-// console.log('admin is true');
-// hc.adminDashboard=true;
-// hc.userDashboard=false;
-// hc.registerForm = false;
-// hc.loginForm = false;
-// }else{
-// console.log('admin is not true here in logout');
-// hc.userDashboard=true;
-// hc.adminDashboard=false;
-// hc.registerForm=false;
-// hc.loginForm=false;
-// }
-// }
-// }, function(response){
-// console.log('unsuccessful logout');
-// });
-// };
 
 
   console.log('Home controller loaded.');
