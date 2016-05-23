4aed9c13cdd2ad59108ba48b0ae13a1ca2a164c2
sasham43 <sashasemail@gmail.com>
2016-05-23 11:18:26 -0500

    Split out client into multiple Angular files
      The original module file must be loaded first.

---
+++ client/northApp.module.js
@@ -0,0 +1,18 @@
+angular.module('northApp', ['ngRoute', 'leaflet-directive', 'ngMaterial', 'ngMessages', 'ngAnimate']);
+
+
+angular.module('northApp').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: '/views/home.html',
+    controller: 'HomeController',
+    controllerAs: 'hc'
+  })
+  .when('/map', {
+    templateUrl: '/views/map.html',
+    controller: 'MapController',
+    controllerAs: 'mc'
+  });
+
+  $locationProvider.html5Mode(true);
+}]);
