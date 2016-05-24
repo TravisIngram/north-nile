bbcef278209a7b6440a39bf4a9c1f165fe641da6
sasham43 <sashasemail@gmail.com>
2016-05-23 22:41:21 -0500

    Cleaned up code and added comments
    Deleted remaining bits of commented out code, add comments to hopefully elucidate the process, refactored openInfoDrawer to its own function.

--- client/mapController.js
+++ client/mapController.js
@@ -1,9 +1,7 @@
 angular.module('northApp').controller('MapController', ['$scope', 'leafletData', 'leafletMarkerEvents', '$mdBottomSheet', function($scope, leafletData, leafletMarkerEvents, $mdBottomSheet){
   var mc = this;
 
-  mc.lastClicked = {};
-
-  // eventually will be pulled from server/database
+  // test data - eventually will be pulled from server/database
   mc.storedMarkers = {
       m1: {
           lat: 44.996121,
@@ -35,30 +33,32 @@ angular.module('northApp').controller('MapController', ['$scope', 'leafletData',
       }
   };
 
+  // start count at a number higher than any keys present in the object - this ensures no duplicates
   mc.markerSize = Object.keys(mc.storedMarkers).length;
   mc.count = mc.markerSize + 1;
   mc.visibleMarkers = {};
 
+  // filter visibility of markers
   mc.filterMarkers = function(type){
     mc.visibleMarkers = {};
+
     if(type === 'all'){
-      //mc.visibleMarkers = mc.storedMarkers;
       for (marker in mc.storedMarkers){
         mc.storedMarkers[marker].visibility = true;
       }
     } else if (type === 'none'){
-      //mc.visibleMarkers = {};
       for (marker in mc.storedMarkers){
         mc.storedMarkers[marker].visibility = false;
       }
     } else {
-      //mc.visibleMarkers = {};
+      // loop through the list of markers from the database, and toggle visibility based on type
       for (marker in mc.storedMarkers){
         if(mc.storedMarkers[marker].type === type){
           mc.storedMarkers[marker].visibility = !mc.storedMarkers[marker].visibility; // toggle visibility
         }
       }
     }
+
     // loop through storedMarkers and add visible ones to visibleMarkers
     for (marker in mc.storedMarkers){
       if(mc.storedMarkers[marker].visibility){
@@ -67,19 +67,20 @@ angular.module('northApp').controller('MapController', ['$scope', 'leafletData',
       }
     }
 
+    // write changes to map
     angular.extend(mc, {
       markers: mc.visibleMarkers
     });
-    console.log('visiibleMarkers:', mc.visibleMarkers);
   };
 
+  // configure map defaults
   angular.extend(mc, {
         defaults: {
             scrollWheelZoom: false,
             touchZoom: true,
             dragging: true,
             tap: true,
-            tapTolerance: 100
+            tapTolerance: 100 // may not be necessary after angular material skipClickHijack() fix
         },
         center: {
           lat: 44.996121,
@@ -90,65 +91,71 @@ angular.module('northApp').controller('MapController', ['$scope', 'leafletData',
             url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         },
         markers: mc.visibleMarkers
+  });
+
+  mc.lastClicked = {};
+
+  // open infoDrawer on marker Click
+  mc.openInfoDrawer = function(event, args){
+    mc.showInfoDrawer = true;
+
+    // grab last marker clicked to recenter map later
+    mc.lastClicked = args.model;
+
+    // test text
+    mc.markerTitle = mc.lastClicked.title;
+    // test text to force overflow and scrolling
+    mc.dummyText1 = 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.';
+    mc.dummyText2 = 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.';
+    mc.dummyText3 = 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.';
+
+    // update map size - needed if we are shrinking the map to recenter the icon/marker in the top middle
+    mc.mapStyle = {height:'30vh'};
+    leafletData.getMap().then(function(map) {
+      setTimeout(function(){ // may not be best practice - seems to result in the map moving too much
+        map.invalidateSize();
+        console.log('called');
+      }, 200);
     });
 
-    // open infoDrawer on marker Click
-    $scope.$on('leafletDirectiveMarker.map.click', function(event, args){
-      // console.log('clicked a marker:', args, '|event:', event);
-      // console.log('visibleMarkers on click:', mc.visibleMarkers);
+    // this centers the map on the marker clicked
+    angular.extend(mc, {
+      center:{
+        lat: mc.lastClicked.lat,
+        lng: mc.lastClicked.lng,
+        zoom: 15
+      }
+    });
+  };
 
-      mc.showInfoDrawer = true;
-      mc.lastClicked = args.model;
-      mc.markerTitle = mc.lastClicked.title;
-      // mc.mapStyle = {height:'20vh'};
+  // close infoDrawer on map click
+  mc.closeInfoDrawer = function(event, args){
+    mc.showInfoDrawer = false;
 
-      // Angular material bottom sheet testing
-      mc.dummyText1 = 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.';
-      mc.dummyText2 = 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.';
-      mc.dummyText3 = 'Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.';
-      // $mdBottomSheet.show({
-      //   template: '<md-bottom-sheet><md-content stopTouchEvent><h2>' + mc.markerTitle + '</h2><p>' + mc.dummyText1 + '</p><p>' + mc.dummyText2 + '</p><p>' + mc.dummyText3 + '</p></md-content></md-bottom-sheet>'
-      // });
-
-      // update map size
-      leafletData.getMap().then(function(map) {
-        setTimeout(function(){
-          map.invalidateSize();
-          console.log('called');
-        }, 200);
-      });
-
-      angular.extend(mc, {
-        center:{
-          lat: mc.lastClicked.lat,
-          lng: mc.lastClicked.lng,
-          zoom: 15
-        }
-      });
+    // update map size - needed if we are shrinking the map to recenter the icon/marker in the top middle
+    mc.mapStyle = {height:'100vh'};
+    leafletData.getMap().then(function(map) {
+      setTimeout(function(){
+        map.invalidateSize();
+        console.log('called');
+      }, 100);
     });
 
-    // close infoDrawer on map click
-    mc.closeInfoDrawer = function(event, args){
-      mc.showInfoDrawer = false;
+    // this centers the map on the marker that initiated the click that this is 'undoing'
+    angular.extend(mc, {
+      center:{
+        lat: mc.lastClicked.lat,
+        lng: mc.lastClicked.lng,
+        zoom: 15
+      }
+    });
+  };
 
-      mc.mapStyle = {height:'100vh'};
-      leafletData.getMap().then(function(map) {
-        setTimeout(function(){
-          map.invalidateSize();
-          console.log('called');
-        }, 100);
-      });
-      angular.extend(mc, {
-        center:{
-          lat: mc.lastClicked.lat,
-          lng: mc.lastClicked.lng,
-          zoom: 15
-        }
-      });
-    };
+  // bind event handlers
+  $scope.$on('leafletDirectiveMarker.map.click', mc.openInfoDrawer);
+  $scope.$on('leafletDirectiveMap.map.click', mc.closeInfoDrawer);
 
-    $scope.$on('leafletDirectiveMap.map.click', mc.closeInfoDrawer);
-
+  // set all markers to visible on page load
   mc.filterMarkers('all');
   console.log('Map controller loaded.');
 }]);
