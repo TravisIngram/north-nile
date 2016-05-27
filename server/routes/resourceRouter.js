afd5df580d21c46da48f5b0e00691cc7cd4427ec
TravisIngram <ingramtravis@gmail.com>
2016-05-27 16:51:18 -0500

    Update /all queryString with join in resourceRouter.js

--- server/routes/resourceRouter.js
+++ server/routes/resourceRouter.js
@@ -37,7 +37,10 @@ router.get('/all', function(request, response){
     } else {
       var resources = [];
 
-      var queryString = 'SELECT * FROM "resource"';
+      // var queryString = 'SELECT * FROM "resource"';
+
+
+      var queryString = 'SELECT "resource".id, "name", "location", "type", "date_created", "account_id", "is_active", "is_pending", "username" FROM "resource" INNER JOIN "account" ON account.id = resource.account_id';
 
       var query = client.query(queryString);
 
