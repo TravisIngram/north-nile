961f41f983eca246e73e2be619e1ecc4399f4017
sasham43 <sashasemail@gmail.com>
2016-06-06 10:14:57 -0500

    Add in H to greenouse svg path

--- client/mapController.js
+++ client/mapController.js
@@ -40,7 +40,7 @@ var foodDistribution = {
                   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
               };
 var foodHub = {
-                  iconUrl: 'assets/img/GreenhousePerfect.svg',
+                  iconUrl: 'assets/img/GreenHousePerfect.svg',
                   iconSize:     [38, 38], // size of the icon
                   shadowSize:   [50, 64], // size of the shadow
                   iconAnchor:   [19, 19], // point of the icon which will correspond to marker's location
